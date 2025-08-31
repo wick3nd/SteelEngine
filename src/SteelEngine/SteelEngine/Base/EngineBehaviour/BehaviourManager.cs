2795228d5cef237d924a0eceaf898d9385226805
wickend <80926418+wick3nd@users.noreply.github.com>
2025-08-31 15:49:01 +0200

    Create BehaviourManager.cs

---
+++ src/SteelEngine/SteelEngine/Base/EngineBehaviour/BehaviourManager.cs
@@ -0,0 +1,69 @@
+using OpenTK.Windowing.Common;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SteelEngine
+{
+    public static class BehaviourManager
+    {
+        private static readonly List<EngineScript> behaviours = [];
+
+        public static void Add(EngineScript script) => behaviours.Add(script);    // EngineScript methods to run
+
+        private static readonly Type[] _engineScriptTypes = [.. Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(EngineScript)) && !t.IsAbstract)];    // EngineScripts to run
+        public static void InitializeES()
+        {
+            for (int i = 0; i < _engineScriptTypes.Length; i++)
+            {
+                var ctor = _engineScriptTypes[i].GetConstructor(Type.EmptyTypes);
+                if (ctor != null)
+                {
+                    var newExpr = Expression.New(ctor);
+                    var lambda = Expression.Lambda<Func<EngineScript>>(newExpr).Compile();
+                    lambda();
+                }
+            }
+        }
+
+        public static void ExposeWidth(int width)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].WindowWidth = width;
+        }
+        public static void ExposeHeight(int height)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].WindowWidth = height;
+        }
+        public static void StartCall()
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].OnStart();
+        }
+        public static void ExitCall()
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].OnExit();
+        }
+        public static void ResizeCall(ResizeEventArgs e)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].OnResize(e);
+        }
+        public static void FrameBufferResizeCall(FramebufferResizeEventArgs e)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].OnFrameBufferResize(e);
+        }
+        public static void FrameUpdateCall(FrameEventArgs e)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].OnFrameUpdate(e);
+        }
+        public static void UpdateCall(FrameEventArgs e)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].Update(e);
+        }
+        public static void LateUpdateCall(FrameEventArgs e)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].LateUpdate(e);
+        }
+        public static void FixedUpdateCall(FrameEventArgs e)
+        {
+            for (int i = 0; i < behaviours.Count; i++) behaviours[i].FixedUpdate(e);
+        }
+    }
+}
