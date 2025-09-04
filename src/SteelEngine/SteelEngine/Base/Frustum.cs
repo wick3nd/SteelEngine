d14c7ca556a315a123ab450ae91bd45f0aba5483
wickend <80926418+wick3nd@users.noreply.github.com>
2025-09-04 11:30:47 +0200

    things i forgot about

--- src/SteelEngine/SteelEngine/Base/Frustum.cs
+++ src/SteelEngine/SteelEngine/Base/Frustum.cs
@@ -6,9 +6,9 @@ namespace SteelEngine.SteelEngine.Base
     {
         public Plane[] planes = new Plane[6];
 
-        public void Update(Matrix4 viewProjection)
+        public void Update(Matrix4 viewProjection)    // use EngineScript update
         {
-            // lewa
+            // left plane
             planes[0] = new Plane(
                 viewProjection.M14 + viewProjection.M11,
                 viewProjection.M24 + viewProjection.M21,
@@ -16,7 +16,7 @@ namespace SteelEngine.SteelEngine.Base
                 viewProjection.M44 + viewProjection.M41
             ).Normalize();
 
-            // prawa
+            // right plane
             planes[1] = new Plane(
                 viewProjection.M14 - viewProjection.M11,
                 viewProjection.M24 - viewProjection.M21,
@@ -24,7 +24,7 @@ namespace SteelEngine.SteelEngine.Base
                 viewProjection.M44 - viewProjection.M41
             ).Normalize();
 
-            // dolna
+            // lower plane
             planes[2] = new Plane(
                 viewProjection.M14 + viewProjection.M12,
                 viewProjection.M24 + viewProjection.M22,
@@ -32,7 +32,7 @@ namespace SteelEngine.SteelEngine.Base
                 viewProjection.M44 + viewProjection.M42
             ).Normalize();
 
-            // gorna
+            // upper plane
             planes[3] = new Plane(
                 viewProjection.M14 - viewProjection.M12,
                 viewProjection.M24 - viewProjection.M22,
@@ -40,7 +40,7 @@ namespace SteelEngine.SteelEngine.Base
                 viewProjection.M44 - viewProjection.M42
             ).Normalize();
 
-            // ta bliska
+            // near plane
             planes[4] = new Plane(
                 viewProjection.M14 + viewProjection.M13,
                 viewProjection.M24 + viewProjection.M23,
@@ -48,29 +48,28 @@ namespace SteelEngine.SteelEngine.Base
                 viewProjection.M44 + viewProjection.M43
             ).Normalize();
 
-            // ta z tylu
+            // far plane
             planes[5] = new Plane(
                 viewProjection.M14 - viewProjection.M13,
                 viewProjection.M24 - viewProjection.M23,
                 viewProjection.M34 - viewProjection.M33,
                 viewProjection.M44 - viewProjection.M43
             ).Normalize();
-
         }
     }
 
-    public struct Plane(float a, float b, float c, float d)
+    public struct Plane(float a, float b, float c, float d)    // use more informative names perhaps?
     {
         public Vector3 Normal = new(a, b, c);
+        float D = d;
 
         public void Normalize()
         {
-            float length = Normal.Length;
+            float length = Normal.Length;    // its a fixed size - put it in struct to not make new one evey time normalize is called
             Normal /= length;
-            d /= length;
+            D /= length;
         }
 
-        public readonly float DistanceToPoint(Vector3 point) => Vector3.Dot(Normal, point) + d;
+        public readonly float DistanceToPoint(Vector3 point) => Vector3.Dot(Normal, point) + D;
     }
-
 }
