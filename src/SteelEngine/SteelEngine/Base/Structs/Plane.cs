ab1331ee9357be115712b2264149b79f140804d1
wickend <80926418+wick3nd@users.noreply.github.com>
2025-09-04 18:19:39 +0200

    Extracted the Plane class

---
+++ src/SteelEngine/SteelEngine/Base/Structs/Plane.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteelMotion.SteelEngine.Base.Structs
+{
+    internal class Plane
+    {
+    }
+}
