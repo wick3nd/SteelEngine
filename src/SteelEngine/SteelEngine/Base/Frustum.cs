33ee8759d929cbbe620fc435817582d553a3e8ec
wickend <80926418+wick3nd@users.noreply.github.com>
2025-09-04 18:37:29 +0200

    Update Frustum.cs

--- src/SteelEngine/SteelEngine/Base/Frustum.cs
+++ src/SteelEngine/SteelEngine/Base/Frustum.cs
@@ -7,58 +7,59 @@ namespace SteelEngine.SteelEngine.Base
     public class Frustum : EngineScript
     {
         public Plane[] planes = new Plane[6];
-        public Matrix4 viewProjection;
 
         public override void Update(FrameEventArgs e)
         {
             base.Update(e);
+            
+            Matrix4 _viewProjection = Camera.view * Camera.projection;
 
             // left plane
             planes[0] = new Plane(new Vector4(
-                viewProjection.M14 + viewProjection.M11,
-                viewProjection.M24 + viewProjection.M21,
-                viewProjection.M34 + viewProjection.M31,
-                viewProjection.M44 + viewProjection.M41
+                _viewProjection.M14 + _viewProjection.M11,
+                _viewProjection.M24 + _viewProjection.M21,
+                _viewProjection.M34 + _viewProjection.M31,
+                _viewProjection.M44 + _viewProjection.M41
             )).Normalize();
 
             // right plane
             planes[1] = new Plane(new Vector4(
-                viewProjection.M14 - viewProjection.M11,
-                viewProjection.M24 - viewProjection.M21,
-                viewProjection.M34 - viewProjection.M31,
-                viewProjection.M44 - viewProjection.M41
+                _viewProjection.M14 - _viewProjection.M11,
+                _viewProjection.M24 - _viewProjection.M21,
+                _viewProjection.M34 - _viewProjection.M31,
+                _viewProjection.M44 - _viewProjection.M41
             )).Normalize();
 
             // bottom plane
             planes[2] = new Plane(new Vector4(
-                viewProjection.M14 + viewProjection.M12,
-                viewProjection.M24 + viewProjection.M22,
-                viewProjection.M34 + viewProjection.M32,
-                viewProjection.M44 + viewProjection.M42
+                _viewProjection.M14 + _viewProjection.M12,
+                _viewProjection.M24 + _viewProjection.M22,
+                _viewProjection.M34 + _viewProjection.M32,
+                _viewProjection.M44 + _viewProjection.M42
             )).Normalize();
 
             // upper plane
             planes[3] = new Plane(new Vector4(
-                viewProjection.M14 - viewProjection.M12,
-                viewProjection.M24 - viewProjection.M22,
-                viewProjection.M34 - viewProjection.M32,
-                viewProjection.M44 - viewProjection.M42
+                _viewProjection.M14 - _viewProjection.M12,
+                _viewProjection.M24 - _viewProjection.M22,
+                _viewProjection.M34 - _viewProjection.M32,
+                _viewProjection.M44 - _viewProjection.M42
             )).Normalize();
 
             // near plane
             planes[4] = new Plane(new Vector4(
-                viewProjection.M14 + viewProjection.M13,
-                viewProjection.M24 + viewProjection.M23,
-                viewProjection.M34 + viewProjection.M33,
-                viewProjection.M44 + viewProjection.M43
+                _viewProjection.M14 + _viewProjection.M13,
+                _viewProjection.M24 + _viewProjection.M23,
+                _viewProjection.M34 + _viewProjection.M33,
+                _viewProjection.M44 + _viewProjection.M43
             )).Normalize();
 
             // far plane
             planes[5] = new Plane(new Vector4(
-                viewProjection.M14 - viewProjection.M13,
-                viewProjection.M24 - viewProjection.M23,
-                viewProjection.M34 - viewProjection.M33,
-                viewProjection.M44 - viewProjection.M43
+                _viewProjection.M14 - _viewProjection.M13,
+                _viewProjection.M24 - _viewProjection.M23,
+                _viewProjection.M34 - _viewProjection.M33,
+                _viewProjection.M44 - _viewProjection.M43
             )).Normalize();
         }
     }
