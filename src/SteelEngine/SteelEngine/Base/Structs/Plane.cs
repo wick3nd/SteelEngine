25506b65133cad408153a28c2f27575d7a716fc3
wickend <80926418+wick3nd@users.noreply.github.com>
2025-09-05 16:26:54 +0200

    fixed the missing Plane code

--- src/SteelEngine/SteelEngine/Base/Structs/Plane.cs
+++ src/SteelEngine/SteelEngine/Base/Structs/Plane.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK.Mathematics;
 
-namespace SteelMotion.SteelEngine.Base.Structs
+namespace SteelEngine.Base.Structs
 {
-    internal class Plane
+    public readonly struct Plane(Vector4 vec)
     {
+        private readonly Vector4 _vec = vec;
+
+        public readonly Plane Normalize()
+        {
+            float len = _vec.Length;
+            return new Plane(_vec / len);
+        }
+
+        public readonly float DistanceToPoint(Vector3 point) => Vector3.Dot(new Vector3(_vec.X, _vec.Y, _vec.Z), point) + _vec.W;
     }
 }
