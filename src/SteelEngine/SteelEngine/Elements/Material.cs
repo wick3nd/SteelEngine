e2936e97e501fce93d986cdf2d03fe46dcf307ed
wickend <80926418+wick3nd@users.noreply.github.com>
2025-09-14 13:06:52 +0200

    Added Material.sc and SEObject.cs

---
+++ src/SteelEngine/SteelEngine/Elements/Material.cs
@@ -0,0 +1,44 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using SteelEngine;
+using SteelEngine.Base;
+using SteelEngine.SteelEngine.Base;
+
+namespace SteelMotion.SteelEngine.Elements
+{
+    internal class Material : EngineScript
+    {
+        public Shader? Shader { get; set; }
+        public Texture2D? Tex1 { get; set; }
+        public Texture2D? Tex2 { get; set; }
+
+        public void Use()
+        {
+            Shader!.Use();
+
+            Shader.SetMatrix4("projection", Camera.projection);
+            Shader.SetMatrix4("view", Camera.view);
+
+            int unit = 0;
+            if (Tex1 != null)
+            {
+                Tex1.Use(TextureUnit.Texture0 + unit);
+                Shader.SetInt("texture0", unit);
+                unit++;
+            }
+            if (Tex2 != null)
+            {
+                Tex2.Use(TextureUnit.Texture0 + unit);
+                Shader.SetInt("texture1", unit);
+                unit++;
+            }
+        }
+
+        public override void OnExit()
+        {
+            Shader!.Dispose();
+            Tex1?.Dispose();
+            Tex2?.Dispose();
+        }
+    }
+
+}
---
+++ src/SteelEngine/SteelEngine/Elements/SEObject.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK.Mathematics;
+using SteelEngine;
+using SteelEngine.Base;
+
+namespace SteelMotion.SteelEngine.Elements
+{
+    internal class SEObject : EngineScript
+    {
+        public Mesh? Mesh { get; set; }
+        public Material? Material { get; set; }
+
+        public void DrawInstanced(Matrix4[] instanceMatrix)
+        {
+            Material!.Use();
+            Mesh!.DrawInstanced(instanceMatrix);
+        }
+
+        public override void OnExit()
+        {
+            Mesh?.Dispose();
+        }
+    }
+}
