87358217ecfc46dcd64eb1ac2fdaa2fc02e4fe66
wickend <80926418+wick3nd@users.noreply.github.com>
2025-10-17 18:36:57 +0200

    Update Material.cs
    removed the redundant code

--- src/SteelEngine/SteelEngine/Elements/Material.cs
+++ src/SteelEngine/SteelEngine/Elements/Material.cs
@@ -1,44 +1,31 @@
-﻿using OpenTK.Graphics.OpenGL4;
-using SteelEngine;
+using OpenTK.Graphics.OpenGL;
 using SteelEngine.Base;
-using SteelEngine.SteelEngine.Base;
 
 namespace SteelMotion.SteelEngine.Elements
 {
-    internal class Material : EngineScript
+    internal class Material
     {
         public Shader? Shader { get; set; }
-        public Texture2D? Tex1 { get; set; }
-        public Texture2D? Tex2 { get; set; }
+        public Texture2D? Texture1 { get; set; }
+        public Texture2D? Texture2 { get; set; }
 
         public void Use()
         {
-            Shader!.Use();
-
-            Shader.SetMatrix4("projection", Camera.projection);
-            Shader.SetMatrix4("view", Camera.view);
+            Shader!.Enable();
 
             int unit = 0;
-            if (Tex1 != null)
+            if (Texture1 != null)
             {
-                Tex1.Use(TextureUnit.Texture0 + unit);
+                Texture1.Use(TextureUnit.Texture0 + unit);
                 Shader.SetInt("texture0", unit);
                 unit++;
             }
-            if (Tex2 != null)
+            if (Texture2 != null)
             {
-                Tex2.Use(TextureUnit.Texture0 + unit);
+                Texture2.Use(TextureUnit.Texture0 + unit);
                 Shader.SetInt("texture1", unit);
                 unit++;
             }
         }
-
-        public override void OnExit()
-        {
-            Shader!.Dispose();
-            Tex1?.Dispose();
-            Tex2?.Dispose();
-        }
     }
-
 }
